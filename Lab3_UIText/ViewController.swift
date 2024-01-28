6a2d8a21e92062cd1cb01514fb7f0563153f6dda
rameshsdeveloper <jeniramesh142@gmail.com>
2024-01-28 00:57:13 -0500

    Initial Commit

---
+++ Lab3_UIText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab3_UIText
+//
+//  Created by user236599 on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
